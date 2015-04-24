cabcf3b26058e9a7b3c7c8e92130ebc2bab7dcb7
Davin Kevin <davin.kevin@gmail.com>
2015-04-24 05:54:42 +0200

    feat(warmup.halloweenparty) implementation of Halloween Party problem (https://www.hackerrank.com/challenges/halloween-party)

---
+++ src/main/java/com/hackerrank/challenges/warmup/halloweenparty/Solution.java
@@ -0,0 +1,26 @@
+package com.hackerrank.challenges.warmup.halloweenparty;
+
+import java.util.Scanner;
+
+/**
+ * Created by kevin on 18/04/15 for HackerRank problem
+ * https://www.hackerrank.com/challenges/halloween-party
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // Don't use the number of occurence, useless
+        in.nextInt();
+
+        while(in.hasNextInt()) {
+            System.out.println(nbOfChocolatePiece(in.nextLong()));
+        }
+    }
+
+    public static Long nbOfChocolatePiece(Long k) {
+        return k / 2 * (k - k / 2);
+    }
+
+}
